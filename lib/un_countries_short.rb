86c1e08f15584cdbe252420f9c95d6123c20155f
Spiros Avlonitis <spirosa84@hotmail.com>
2017-02-16 18:50:15 +0200

    fixed countries

--- lib/un_countries_short.rb
+++ lib/un_countries_short.rb
@@ -1,5 +1,29 @@
 require "un_countries_short/version"
 
 module UnCountriesShort
-  # Your code goes here...
+  
+  	def self.countries_list
+
+  		  countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Antigua and Barbuda",
+    	"Argentina","Armenia","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh",
+    	"Barbados","Belarus","Belgium","Belize","Benin","Bhutan","Bolivia","Bosnia and Herzegovina",
+    	"Botswana","Brazil","Brunei Darussalam","Bulgaria","Burkina Faso","Burundi","Cabo Verde","Cambodia",
+    	"Cameroon","Canada","Central African Republic","Chad","Chile","China","Colombia","Comoros","Congo",
+    	"Costa Rica","Côte d'Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Rep Congo",
+    	"Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea",
+    	"Estonia","Ethiopia","Fiji","Finland","France","FYROM","Gabon","Gambia","Georgia","Germany","Ghana","Greece","Grenada","Guatemala",
+    	"Guinea","Guinea-Bissau","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel",
+    	"Italy","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Kuwait","Kyrgyzstan","Lao","Latvia",
+    	"Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta",
+    	"Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Monaco","Mongolia","Montenegro","Morocco","Mozambique","Myanmar",
+    	"Namibia","Nauru","Nepal","Netherlands","New Zealand","Nicaragua","Niger","Niger","North Korea","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay",
+    	"Peru","Philippines","Poland","Portugal","Qatar","Republic of Moldova","Romania","Russian Federation","Rwanda","Saint Kitts and Nev",
+    	"Saint Lucia","Saint Vincent","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone",
+    	"Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","Sudan","Suriname",
+    	"Swaziland","Sweden","Switzerland","Syrian Arab Republic","Tajikistan","Thailand","Timor-Leste","Togo",
+    	"Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","Tanzania",
+    	"United States of America","Uruguay","Uzbekistan","Vanuatu","Venezuela","Viet Nam","Yemen","Zambia","Zimbabwe"]
+  		
+  	end
+
 end
